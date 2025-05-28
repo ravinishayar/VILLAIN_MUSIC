150459d488fd0e26b94c9f22af4112cb9d745489
ravinishayar <remixoldhindi@gmail.com>
2025-05-28 13:16:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,10 +27,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002311769574))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002600355202))
 
 # Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 5909658683))
+OWNER_ID = int(getenv("OWNER_ID", 6308382997))
 
 
 # make your bots privacy from telegra.ph and put your url here 
